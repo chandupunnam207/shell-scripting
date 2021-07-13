a0ef35c4ef4d24fc513253e1a3aa3e8e7c9e7641
Punnam <chandu.punnam207@gmail.com>
2021-07-14 00:04:13 +0530

    Printing hello world

--- print.sh
+++ print.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-echo -e '\e[31mHello World, \n\t This is my print page\e'
+echo -e '\e[31mHello World, \n This is my print page\e'
+
+echo -e '\e[35 My name is Linux, What is your name\e'
