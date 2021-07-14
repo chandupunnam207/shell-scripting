b8d6b905b5d8d03808fd114cf550317a2bdb308b
Punnam <chandu.punnam207@gmail.com>
2021-07-15 00:57:42 +0530

    Verifying Input command

--- 004input.sh
+++ 004input.sh
@@ -4,7 +4,7 @@ read -p "What is your name?" name
 echo Your name is $name
 
 read -p "Enter your Aadhar Card Number" Aadharcardverified
-echo $Aadharcardverified
+echo Your Aadhar Card $Aadharcardverified verified
 
 read -p "Enter passport number" Passportverified
-echo $Passportverified
+echo Your Passport number $Passportverified verified
