d498af820556b6e7d9148b853adc10ac2477fdcb
Punnam <chandu.punnam207@gmail.com>
2021-07-15 00:55:28 +0530

    printing vars

--- 004input.sh
+++ 004input.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 
 read -p "What is your name?" name
-echo my name is $name
+echo Your name is $name
+
+read -p "Aadhar Card Number" Aadharcardverified
+echo $Aadharcardverified
+
+read -p "Enter passport number" Passportverified
+echo $Passportverified
