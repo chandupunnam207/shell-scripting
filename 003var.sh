5c048cb6fffcd6ed7b3bd3763e3790b0f627873d
Punnam <chandu.punnam207@gmail.com>
2021-07-15 00:26:33 +0530

    printing vars

--- 003var.sh
+++ 003var.sh
@@ -15,5 +15,5 @@ Output_files=$(cat 001Shell.sh)
 echo Here is the output for your shell = $Output_files
 
 
-a=(10 20)
-echo ${a[1]}
+a=(10 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800)
+echo ${a[12]}
