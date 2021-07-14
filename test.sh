4f7c3b66231d147e75a6e457c1dc3235b997bf44
Punnam <chandu.punnam207@gmail.com>
2021-07-14 23:16:53 +0530

    printing colurs

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 echo -e '\e[31mHello World, \n This is my print page\e'
 
 echo -e "\e[32mMessage in Green\e"
-echo -e '\e[33mMessage in Yellow\e'
-echo -e '\e[34mMessage in Blue\e'
+echo -e "\e[33mMessage in Yellow\e"
+echo -e "\e[34mMessage in Blue\e"
