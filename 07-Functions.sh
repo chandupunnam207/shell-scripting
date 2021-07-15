72491419f7ab6881acb87909b6de19369b59221c
Punnam <chandu.punnam207@gmail.com>
2021-07-15 23:51:58 +0530

    Input shell testing

--- 07-Functions.sh
+++ 07-Functions.sh
@@ -9,14 +9,16 @@ Testing() {
   echo value of d=${d}
   echo value of e=${e}
   echo value of f=${f}
+  echo First argument of this program=$*
 }
 
 d=400
 e=500
 f=600
-Testing
+Testing This is a good example
 echo value of a=$a
 echo value of b=$b
 echo value of c=$c
+echo First argument of the main program=$@
 
 
