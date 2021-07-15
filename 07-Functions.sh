fd688668b659a44ee463ede3b0b1df449592edfb
Punnam <chandu.punnam207@gmail.com>
2021-07-15 23:44:21 +0530

    Input shell testing

--- 07-Functions.sh
+++ 07-Functions.sh
@@ -6,16 +6,17 @@ Testing() {
   a=100
   b=200
   c=300
-  echo value of d=$d
-  echo value of e=$e
-  echo value of f=$f
+  echo value of d = ${d}
+  echo value of e = ${e}
+  echo value of f = ${f}
 }
+
+d=400
+e=500
+f=600
 
 Testing
 echo value of a=$a
 echo value of b=$b
 echo value of c=$c
-d=400
-e=500
-f=600
 
