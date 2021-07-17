35132e1329475eb21e3b42d38585ccb5fc442dbb
Punnam <chandu.punnam207@gmail.com>
2021-07-17 23:24:06 +0530

    Testing shell scripting for echo commands on linux

--- TestingShell.sh
+++ TestingShell.sh
@@ -8,6 +8,6 @@ echo My shell scripting needs code
 
 echo -e "\e[35mMy command is printed correctly \nThank you\e[0m"
 echo -e "\e[31mApple is red in color\e[0m"
-echo -e "\e[32I like green tea\e[0m"
+echo -e "\e[32mI like green tea\e[0m"
 echo -e "\e[33mMy chair color is yellow\e[0m"
 echo -e "\e[36m*\n**\n***\n****\n*****\e[0m"
