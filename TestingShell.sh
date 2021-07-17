3004f1ff765a10f56acb83d65d525243548c0a89
Punnam <chandu.punnam207@gmail.com>
2021-07-17 23:17:51 +0530

    Testing shell scripting for echo commands on linux

--- TestingShell.sh
+++ TestingShell.sh
@@ -3,12 +3,11 @@
 type bash
 ls
 ls -ls
-cat 01-Shell.sh
 
 echo Hello India
 echo My shell scripting needs code
 
-echo -e "My command is printed correctly \nThank you"
+echo -e "\e[35mMy command is printed correctly \nThank you\e[0m"
 echo -e "\e[31mApple is red in color\e[0m"
 echo -e "\e[32mMy car color is green\e[0m"
 echo -e "\e[33mMy chair color is yellow\e[0m"
