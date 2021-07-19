47435bc4fa9a99124e2994835bc29fddd5c3c2d2
Punnam <chandu.punnam207@gmail.com>
2021-07-20 01:43:25 +0530

    Frontend shell testing

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,28 +6,27 @@ rm -f $LOG
 echo -e "Installing frontend"
 yum install nginx -y >>$LOG
 if [ $? -eq 0 ]; then
-    echo done
+    echo "done"
   else
-    echo fail
+    echo "fail"
 fi
-}
+
 
 
 echo -e "Enabling nginx"
 systemctl enable nginx >>$LOG
-if [ $? eq 0 ]; then
-    echo done
+if [ $? -eq 0 ]; then
+    echo "done"
   else
-    echo fail
+    echo "fail"
 fi
-}
+
 
 
 echo -e "Starting nginx"
 systemctl start nginx >>$LOG
-if [ $? eq 0 ]; then
-    echo done
+if [ $? -eq 0 ]; then
+    echo "done"
   else
-    echo fail
+    echo "fail"
 fi
-}
