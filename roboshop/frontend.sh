eb5ba247af7a05b6e495b249a16e6f66b4ee5ef8
Punnam <chandu.punnam207@gmail.com>
2021-07-20 01:18:35 +0530

    Frontend shell testing

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -5,8 +5,8 @@ LOG=/tmp/roboshop.log
 echo -e "Installing frontend ...\e[32mdone\e[0m"
 yum install nginx -y >>$LOG
 
-echo "Enabling nginx ...\e[32mdone\e[0m"
+echo -e "Enabling nginx ...\e[32mdone\e[0m"
 systemctl enable nginx >>$LOG
 
-echo "Starting nginx ...\e[32mdone\e[0m"
+echo -e "Starting nginx ...\e[32mdone\e[0m"
 systemctl start nginx >>$LOG
