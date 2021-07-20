4a674ba8da2d15d48ea2d6907c56afb323b7793e
Punnam <chandu.punnam207@gmail.com>
2021-07-20 23:46:32 +0530

    Frontend shell testing

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -7,22 +7,22 @@ rm -f $LOG
 
 STAT_CHECK() {
   if [ $1 -eq 0 ]; then
-     echo -e "\e[32mdone\e[0m"
+     echo -e "\e[32m\tdone\e[0m"
   else
      echo -e "\e[31mfail\e[0m"
      exit 1
   fi
 }
 
-echo -n -e "Installing frontend"
+echo -n -e "Installing frontend..."
 yum install nginx -y >>$LOG
 STAT_CHECK $?
 
 
-echo -n -e "Enabling nginx"
+echo -n -e "Enabling nginx..."
 systemctl enable nginx >>$LOG
 STAT_CHECK $?
 
-echo -n -e "Starting nginx"
+echo -n -e "Starting nginx..."
 systemctl start nginx >>$LOG
 STAT_CHECK $?
