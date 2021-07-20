f3e0d0400d111024bc39e3345bf7dd4fbe9fc84b
Punnam <chandu.punnam207@gmail.com>
2021-07-20 23:59:00 +0530

    Frontend shell testing

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -1,5 +1,11 @@
 #!usr/bin/bash
 
+USER_ADD=$(id -u)
+if [USER_ADD ne 0] ; then
+  echo -e "\e[31mYou should be a root user or sudo user to run this command\e[0m"
+fi
+
+
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
