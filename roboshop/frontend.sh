e9afc25571644121a1906b2b7704c0b37b6a809a
Punnam <chandu.punnam207@gmail.com>
2021-07-23 23:47:10 +0530

    Frontend shell testing

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,14 +6,6 @@ PRINT "Installing frontend\t"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Enabling nginx\t\t"
-systemctl enable nginx &>>$LOG
-STAT_CHECK $?
-
-PRINT "Starting nginx\t\t"
-systemctl start nginx &>>$LOG
-STAT_CHECK $?
-
 PRINT "Download Frontend\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STAT_CHECK $?
@@ -30,8 +22,12 @@ PRINT "Update Roboshop Configuration"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
-PRINT "RESTART NGINX"
-systemctl restart nginx
+PRINT "Enabling nginx\t\t"
+systemctl enable nginx &>>$LOG
+STAT_CHECK $?
+
+PRINT "Starting nginx\t\t"
+systemctl start nginx &>>$LOG
 STAT_CHECK $?
 
 
