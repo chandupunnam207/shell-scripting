5f2d6a0cdf93e2cec7195963c40e9f094fdeeb21
Punnam <chandu.punnam207@gmail.com>
2021-07-23 23:43:33 +0530

    Frontend shell testing

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,27 +2,27 @@
 
 source common.sh
 
-PRINT "Installing frontend"
+PRINT "Installing frontend\t"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Enabling nginx\t"
+PRINT "Enabling nginx\t\t"
 systemctl enable nginx &>>$LOG
 STAT_CHECK $?
 
-PRINT "Starting nginx\t"
+PRINT "Starting nginx\t\t"
 systemctl start nginx &>>$LOG
 STAT_CHECK $?
 
-PRINT "Download Frontend"
+PRINT "Download Frontend\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Remove Old HTDocs"
+PRINT "Remove Old HTDocs\t"
 cd /usr/share/nginx/html && rm -rf * &>>$LOG
 STAT_CHECK $?
 
-PRINT "Extract Frontend Zip"
+PRINT "Extract Frontend Zip\t"
 unzip /tmp/frontend.zip &>>$LOG  && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
 STAT_CHECK $?
 
