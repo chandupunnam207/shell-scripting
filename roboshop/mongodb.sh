adb0a3a4098459180cc491aa37229035106c4fb9
Punnam <chandu.punnam207@gmail.com>
2021-07-24 00:19:34 +0530

    Frontend shell testing

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -11,7 +11,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STAT_CHECK $?
 
-PRINT "Installing mongodb"
+PRINT "Installing mongodb\t"
 yum install -y mongodb-org &>>$LOG
 STAT_CHECK $?
 
@@ -19,15 +19,15 @@ PRINT "Update mongodb IP in config"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 STAT_CHECK $?
 
-PRINT "Enable MongoDB\t"
+PRINT "Enable MongoDB\t\t"
 systemctl enable mongod &>>$LOG && systemctl start mongod &>>$LOG
 STAT_CHECK $?
 
-PRINT "Download mongodb schema"
+PRINT "Download mongodb schema\t"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"&>>$LOG
 STAT_CHECK $?
 
-PRINT ""
+PRINT "Download mongodb schema\t"
 cd /tmp && unzip mongodb.zip&>>$LOG $$ cd mongodb-main && mongo < catalogue.js&>>$LOG && mongo < users.js&>>$LOG
 STAT_CHECK $?
 
