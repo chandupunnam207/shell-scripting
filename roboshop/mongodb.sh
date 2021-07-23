46b694a8bbe645fbae382421f2ce91f34d003d7b
Punnam <chandu.punnam207@gmail.com>
2021-07-24 00:24:30 +0530

    Frontend shell testing

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-PRINT "Setting up repository"
+PRINT "Setting up repository\t"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -19,7 +19,7 @@ PRINT "Update mongodb IP in config"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 STAT_CHECK $?
 
-PRINT "Enable MongoDB\t\t"
+PRINT "Start & enable MongoDB\t\t"
 systemctl enable mongod &>>$LOG && systemctl start mongod &>>$LOG
 STAT_CHECK $?
 
@@ -27,8 +27,8 @@ PRINT "Download mongodb schema\t"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"&>>$LOG
 STAT_CHECK $?
 
-PRINT "Download mongodb schema\t"
-cd /tmp && unzip mongodb.zip &>>$LOG $$ cd mongodb-main && mongo < catalogue.js &>>$LOG && mongo < users.js &>>$LOG
+PRINT "Load mongodb schema\t"
+cd /tmp && unzip -o mongodb.zip &>>$LOG && cd mongodb-main && mongo < catalogue.js &>>$LOG && mongo < users.js &>>$LOG
 STAT_CHECK $?
 
 
