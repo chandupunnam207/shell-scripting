3ebb172d575a2bcc835c113c7187343eb5106ef7
Punnam <chandu.punnam207@gmail.com>
2021-07-24 19:26:21 +0530

    Updated the Add Roboshop code

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -18,7 +18,7 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 STAT_CHECK $?
 
 PRINT "Extract downloaded code\t"
-cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue &>>$LOG && cd /home/roboshop/catalogue && npm install --unsafe-perm&>>$LOG
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue &>>$LOG && cd /home/roboshop/catalogue && npm install --unsafe-perm&>>$LOG
 STAT_CHECK $?
 
 PRINT "Update service file"
