b6bac84163061d7fe3ff8175d98516448c337703
Punnam <chandu.punnam207@gmail.com>
2021-07-25 18:12:25 +0530

    Updated the Add Roboshop code

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -57,7 +57,8 @@ NODEJS() {
   STAT_CHECK $?
 
   PRINT "Update Systemd File\t"
-  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/"
+  /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
   STAT_CHECK $?
 
 
